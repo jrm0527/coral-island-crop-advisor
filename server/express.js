4d63784b065e473b99e73c236053c08f2436db41
jrm0527 <geeziegames@gmail.com>
2023-04-10 19:45:13 -0400

    added update crop route

--- server/express.js
+++ server/express.js
@@ -80,6 +80,36 @@ app.post("/api/crops", async (req, res, next) => {
   client.end();
 });
 
+app.put("/api/crops/:cropId", async (req, res, next) => {
+  let id = req.params.cropId;
+  let cropName = req.body.name;
+  let growth = req.body.growth_time;
+  let regrowth_time = req.body.regrowth_time;
+  let regrowth = false;
+  let seed = req.body.seed_price;
+  let sell = req.body.sell_price;
+  let season = req.body.season;
+
+  if (regrowth > 0) {
+    regrowth = true;
+  }
+
+  const query = {
+    text: `UPDATE crop SET (name, growth_time, regrowth, regrowth_time, seed_price, sell_price, season) = ($1, $2, $3, $4, $5, $6, $7) WHERE id = ${id} RETURNING *`,
+    values: [cropName, growth, regrowth, regrowth_time, seed, sell, season],
+  };
+
+  const client = new Client(process.env.DATABASE_URL);
+  client.connect();
+  const { rows } = await client.query(query);
+  if (rows.length === 0) {
+    next({ status: 400, message: "Bad Request" });
+  } else {
+    res.send(rows);
+  }
+  client.end();
+});
+
 app.delete("/api/crops/:cropIndex", async function (req, res, next) {
   let index = req.params.cropIndex;
   const query = {
