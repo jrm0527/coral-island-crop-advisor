b0075b16649a37d7830b5db21d3d8ab4d039675e
jrm0527 <geeziegames@gmail.com>
2023-04-10 17:50:02 -0400

    deployment ready

--- sql/migration.sql
+++ sql/migration.sql
@@ -4,8 +4,8 @@ CREATE TABLE crop (
     id serial PRIMARY KEY,
     name varchar(30),
     growth_time int,
-    regrowth: boolean,
-    regrowth_time: int,
+    regrowth boolean,
+    regrowth_time int,
     seed_price int,
     sell_price int,
     season varchar (30)
